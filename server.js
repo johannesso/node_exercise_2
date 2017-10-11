5b5862b965f3fd9a7f1bc3a46213e5349d013c66
johannesso <johannes_skrede@hotmail.com>
2017-10-11 21:03:46 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static('./public'))
+
+app.get('/:id', function(request, response) {
+  response.send('Hallo ' + request.params.id);
+});
+
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!')
+})
